5df56e7e4822cba00f91631c07c77c1796a0f5a4
Debora <deboramasciave@gmail.com>
2025-02-19 15:58:27 +0100

    images delete fixed

--- src/main/java/it/aulab/chronicles/Repository/ImageRepository.java
+++ src/main/java/it/aulab/chronicles/Repository/ImageRepository.java
@@ -9,6 +9,6 @@ import it.aulab.chronicles.Model.Image;
 
 public interface ImageRepository extends JpaRepository<Image, Long> {
     @Modifying
-    @Query(value= "DELETE FROM image WHERE path = :path", nativeQuery = true)
+    @Query(value= "DELETE FROM images WHERE path = :path", nativeQuery = true)
     void deleteByPath(@Param("path") String path);
 }
